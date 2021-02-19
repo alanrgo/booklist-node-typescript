9eb1d57349805ef8e4d9d505ed95d4268e49c02b
Alan Gomes <alan.gomes@wildlifestudios.com>
2021-02-18 21:47:09 -0300

    feat: add update book controller

--- src/controllers/books/controller.spec.ts
+++ src/controllers/books/controller.spec.ts
@@ -1,13 +1,16 @@
 import BookController from "./controller"
 import { mockRequest, mockResponse } from 'jest-mock-req-res';
+import { Book } from "../../models/Book";
 
 const mockGetBookList = jest.fn()
 const mockAddBook = jest.fn()
+const mockUpdateBook = jest.fn()
 jest.mock('../../repository/book-repository', () => {
     return jest.fn().mockImplementation(() => {
         return {
             getBookList: mockGetBookList,
-            addBook: mockAddBook
+            addBook: mockAddBook,
+            updateBook: mockUpdateBook
         }
     })
 })
@@ -85,6 +88,35 @@ describe('Books Controller', () => {
             controller.addBookController(req, res, next)
     
             expect(mockAddBook).toBeCalledWith(bookData)
+        })
+    })
+
+    describe('updateBookController', () => {
+
+        it('should call the correct repository function when calling updateBookController', () => {
+            const controller = new BookController();
+            const bookData = {
+                title: "title",
+                description: "description",
+                id: 1
+            }
+            const req = mockRequest({body: bookData})
+            const res = mockResponse()
+            const next = {}
+            controller.updateBookController(req, res, next)
+    
+            expect(mockUpdateBook).toBeCalledTimes(1)
+            expect(mockUpdateBook).toBeCalledWith(bookData as Book)
+        })
+    
+        it('should send json payload to client', async () => {
+            const controller = new BookController();
+            const req = mockRequest()
+            const res = mockResponse()
+            const next = {}
+            await controller.updateBookController(req, res, next)
+    
+            expect(res.sendStatus).toBeCalledWith(200)
         })
     })
 })
--- src/controllers/books/controller.ts
+++ src/controllers/books/controller.ts
@@ -1,22 +1,34 @@
 import {Request, Response} from 'express';
+import { Book } from '../../models/Book';
 import BookRepository from '../../repository/book-repository';
 import { BookParams } from './interfaces/book-params';
 
 export default class BookController {
     
     public async getBooksController(req: Request, res: Response, next: any): Promise<void> {
-        const books = await new BookRepository().getBookList()
+        const books = await new BookRepository().getBookList();
         res.header("Content-type", "application/json");
         res.status(200).json(books);
     }
 
     public async addBookController(req: Request, res: Response, next: any) {
-        const { title } = req.body
-        const { description } = req.body
-        const bookData: BookParams = { title, description }
+        const { title } = req.body;
+        const { description } = req.body;
+        const bookData: BookParams = { title, description };
         
-        const addedBook = await new BookRepository().addBook(bookData)
+        const addedBook = await new BookRepository().addBook(bookData);
         res.header("Content-type", "application/json");
         res.status(200).json(addedBook);
+    }
+
+    public async updateBookController(req: Request, res: Response, next: any): Promise<void> {
+        const { title } = req.body;
+        const { description } = req.body;
+        const { id } = req.body;
+
+        const book: Book = {title, description, id}
+        await new BookRepository().updateBook(book);
+        res.header("Content-type", "application/json");
+        res.sendStatus(200);
     }
 }
