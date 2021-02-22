7d4dc3de8eb6e161ccf295903cdb3dca00047cf5
Alan Gomes <alan.gomes@wildlifestudios.com>
2021-02-22 09:22:04 -0300

    feat: error handling so the application does not break

--- src/controllers/books/controller.ts
+++ src/controllers/books/controller.ts
@@ -6,38 +6,58 @@ import { BookParams } from './interfaces/book-params';
 export default class BookController {
     
     public async getBooksController(req: Request, res: Response, next: any): Promise<void> {
-        const books = await new BookRepository().getBookList();
-        res.header("Content-type", "application/json");
-        res.status(200).json(books);
+        try {
+            const books = await new BookRepository().getBookList();
+            res.header("Content-type", "application/json");
+            res.status(200).json(books);
+        } catch (err) {
+            console.error(err);
+            res.status(500);
+        }
     }
 
     public async addBookController(req: Request, res: Response, next: any) {
-        const { title } = req.body;
-        const { description } = req.body;
-        const bookData: BookParams = { title, description };
-        
-        const addedBook = await new BookRepository().addBook(bookData);
-        res.header("Content-type", "application/json");
-        res.status(200).json(addedBook);
+        try {
+            const { title } = req.body;
+            const { description } = req.body;
+            const bookData: BookParams = { title, description };
+            
+            const addedBook = await new BookRepository().addBook(bookData);
+            res.header("Content-type", "application/json");
+            res.status(200).json(addedBook);
+        } catch (err) {
+            console.error(err);
+            res.status(500);
+        }
     }
 
     public async updateBookController(req: Request, res: Response, next: any): Promise<void> {
-        const { title } = req.body;
-        const { description } = req.body;
-        const { id } = req.body;
-
-        const book: Book = {title, description, id}
-        await new BookRepository().updateBook(book);
-        res.header("Content-type", "application/json");
-        res.status(200).json({});
+        try {
+            const { title } = req.body;
+            const { description } = req.body;
+            const { id } = req.body;
+    
+            const book: Book = {title, description, id}
+            await new BookRepository().updateBook(book);
+            res.header("Content-type", "application/json");
+            res.status(200).json({});
+        } catch(err) {
+            console.error(err);
+            res.status(500);
+        }
     }
 
     public async deleteBookController(req: Request, res: Response, next: any): Promise<void> {
-        const { id } = req.body;
+        try {
+            const { id } = req.body;
 
-        await new BookRepository().deleteBook(id);
-        res.header("Content-type", "application/json");
-        res.status(200).json({});
+            await new BookRepository().deleteBook(id);
+            res.header("Content-type", "application/json");
+            res.status(200).json({});
+        } catch(err) {
+            console.error(err);
+            res.status(500);
+        }
     }
     
 }
